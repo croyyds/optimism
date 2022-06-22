84f4db0d0b5168e1dde8aa12566571d8e1ac6cbf
Joshua Gutow <jgutow@optimism.io>
2022-06-22 11:18:50 -0700

    Eager Batch Derivation
    Does the following:
    - Split the batch queue into a batch queue & payload attributes queue
    - Change batch derivation rules to enable eager batch derivation
    - Eagerly create payload attributes

--- op-node/testlog/testlog.go
+++ op-node/testlog/testlog.go
@@ -140,9 +140,10 @@ func (l *logger) flush() {
 	l.t.Helper()
 	// 2 frame skip for flush() + public logger fn
 	decorationLen := estimateInfoLen(2)
-	padding := 20
-	if decorationLen <= 25 {
-		padding = 25 - decorationLen
+	padding := 0
+	targetWidth := 30
+	if decorationLen <= targetWidth {
+		padding = targetWidth - decorationLen
 	}
 	for _, r := range l.h.buf {
 		l.t.Logf("%*s%s", padding, "", l.h.fmt.Format(r))
